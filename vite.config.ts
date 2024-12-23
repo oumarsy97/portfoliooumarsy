0876462eed1c851275ba8d8548fb5642e5d5d99b
oumar SY <syoumar505@gmail.com>
2024-12-23 16:13:39 +0000

    okk

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Définit "@" comme alias pour le dossier "src"
+    },
+  },
   build: {
-    outDir: 'build', // Dossier de sortie pour correspondre à Vercel
+    outDir: 'build', // Correspond à la sortie attendue par Vercel
   },
 });
